428261072b9810bb10d173f5d0c01497f7555809
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2022-11-04 09:39:32 +0100

    components

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+function CardButton() {
+    return <button>Button</button>
+} 
+
+function CardHeader() {
+    return <h2>This is header</h2>
+}
+
+function CardText() {
+    return <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda impedit aperiam odit velit! Expedita est ipsum quos tempore at repellat.</p>
+}
+
+function CardImage() {
+    return <img src="https://placekitten.com/g/230/130" alt="cat" />
+}
+
 function Card() {
     return <div class="my-card">
-        <img src="https://placekitten.com/g/230/130" alt="cat"></img>
-        <h2>This is header</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda impedit aperiam odit velit! Expedita est ipsum quos tempore at repellat.</p>
-        <button>Button</button>
+        <CardImage />
+        <CardHeader />
+        <CardText />
+        <CardButton />
     </div>
 }
 
